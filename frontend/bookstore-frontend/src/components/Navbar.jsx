7a5bcaa07f66885c79eb87ee1fdb345a37f8305b
Uday Shastrakar <uday.shastrakar@gmail.com>
2025-07-07 21:54:25 +0530

    added pages

--- frontend/bookstore-frontend/src/components/Navbar.jsx
+++ frontend/bookstore-frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
+    <nav className="bg-blue-400 text-white p-4 flex justify-between items-center">
       <h1 className="text-xl font-bold">Bookstore</h1>
       <div className="space-x-4">
         <Link to="/" className="hover:underline">Books</Link>
